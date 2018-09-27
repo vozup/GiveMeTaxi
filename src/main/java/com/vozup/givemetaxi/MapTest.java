5f6e01001894020fd757221a2dd0f67d14013a99
vozup <vozupserega@gmail.com>
2018-09-27 21:04:22 +0300

    Add google autocomplete input field

--- src/main/java/com/vozup/givemetaxi/MapTest.java
+++ src/main/java/com/vozup/givemetaxi/MapTest.java
@@ -1,16 +1,44 @@
 package com.vozup.givemetaxi;
 
+import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
+import java.util.HashMap;
+import java.util.Map;
 
 @Named
 public class MapTest {
     private String distanceText;
     private String distanceValue;
+    private Integer priceForOrder;
+
+    private Map<CarType, Integer> priceForKm;
 
     private String from;
     private String to;
+
+//    @PostConstruct
+//    private void init(){
+//        priceForKm = new HashMap<>();
+//        priceForKm.put(CarType.STANDART, 10);
+//        priceForKm.put(CarType.BUISNESS, 20);
+//        priceForKm.put(CarType.COMFORT, 25);
+//        priceForKm.put(CarType.MICROBUS, 25);
+//    }
+
+    public Integer calculatePrice(String carType){
+        Integer price = priceForKm.get(CarType.valueOf(carType));
+        return Integer.getInteger(distanceValue) * price;
+    }
+
+    public Integer getPriceForOrder() {
+        return priceForOrder;
+    }
+
+    public void setPriceForOrder(Integer priceForOrder) {
+        this.priceForOrder = priceForOrder;
+    }
 
     public String getFrom() {
         return from;
--- src/main/java/com/vozup/givemetaxi/views/operator/TakeOrder.java
+++ src/main/java/com/vozup/givemetaxi/views/operator/TakeOrder.java
@@ -7,13 +7,17 @@ import com.vozup.givemetaxi.entities.OrderEntity;
 import com.vozup.givemetaxi.repository.OrderRepository;
 import org.springframework.dao.DataIntegrityViolationException;
 
+import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.validation.constraints.NotNull;
 import java.sql.Time;
 import java.util.Date;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Logger;
 
 @Named
@@ -29,12 +33,24 @@ public class TakeOrder {
     private String clientPhoneNumber;
     private String distanceValue;
     private String distanceText;
+    private Map<CarType, Integer> priceForKm;
+    private Integer price;
     @Inject
     AdditionalService service;
     @Inject
     OrderRepository orderRepository;
     @Inject
     OrderQuery orderQuery;
+
+    @PostConstruct
+    private void init(){
+        priceForKm = new HashMap<>();
+        priceForKm.put(CarType.STANDART, 10);
+        priceForKm.put(CarType.BUISNESS, 20);
+        priceForKm.put(CarType.COMFORT, 25);
+        priceForKm.put(CarType.MICROBUS, 25);
+
+    }
 
     public void actionTakeOrder(){
         additionalService = service.getSelectedAdditionalService();
@@ -69,6 +85,19 @@ public class TakeOrder {
 
         orderQuery.addOrder(orderEntity);
 
+    }
+
+    public void calculatePrice(){
+        Integer priceTemp = priceForKm.get(CarType.valueOf(carType));
+        price = priceTemp * Integer.getInteger(distanceValue);
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
     }
 
     public String getDistanceValue() {
