cfd8c1c4929694f4f196cbc43191238ada197730
vozup <vozupserega@gmail.com>
2018-10-07 13:00:29 +0300

    Adding operators page

--- src/main/java/com/vozup/givemetaxi/repository/OperatorRepository.java
+++ src/main/java/com/vozup/givemetaxi/repository/OperatorRepository.java
@@ -2,7 +2,22 @@ package com.vozup.givemetaxi.repository;
 
 import com.vozup.givemetaxi.entities.OperatorEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface OperatorRepository extends JpaRepository<OperatorEntity, Long> {
     OperatorEntity findByLogin(String login);
+    @Modifying
+    @Query("UPDATE OperatorEntity operator SET " +
+            "operator.id=:id," +
+            "operator.login=:login, " +
+            "operator.password=:password," +
+            "operator.name=:name," +
+            "operator.lastName=:lastname")
+    void updateOperator(@Param("id") Long id,
+                        @Param("login") String login,
+                        @Param("password") String password,
+                        @Param("name") String name,
+                        @Param("lastname") String lastname);
 }
